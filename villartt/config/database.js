dc9b241460db92a7b99cb3eab45b216a87f7640a
IrfDev <irving.dev@icloud.com>
2020-06-13 23:50:45 -0500

    Dockerfile: correct the dockerfile

--- villartt/config/database.js
+++ villartt/config/database.js
@@ -4,17 +4,16 @@ module.exports = ({ env }) => ({
         default: {
             connector: 'mongoose',
             settings: {
+                uri: env('DATABASE_URI'),
                 host: env('DATABASE_HOST', '127.0.0.1'),
                 srv: env.bool('DATABASE_SRV', false),
                 port: env.int('DATABASE_PORT', 27017),
-                database: env('DATABASE_NAME', 'villartt-back-end'),
+                database: env('DATABASE_NAME', 'strapi'),
                 username: env('DATABASE_USERNAME', ''),
                 password: env('DATABASE_PASSWORD', ''),
-                uri: env('DATABASE_URI')
             },
             options: {
-                authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-                ssl: env.bool('DATABASE_SSL', false),
+                ssl: env.bool('DATABASE_SSL', true),
             },
         },
     },
